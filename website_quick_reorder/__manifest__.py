a735982d84e3dd51aad91b25e8d3b5f79182bd7e
Llynix <ataylor@fallsgeek.com>
2024-09-28 07:48:16 -0500

    [Fix] Adjust manifest to add image

--- website_quick_reorder/__manifest__.py
+++ website_quick_reorder/__manifest__.py
@@ -3,10 +3,11 @@
     'version': '16.0.1.0.0',
     'summary': """Add products quickly from sale history to cart """,
     'description': """Add products quickly from sale history to cart """,
-    'author': "Falls Geek",
+    'author': "Anthony Taylor (Falls Geek)",
     'category': 'eCommerce',
     'depends': ['website_sale'],
     'license': 'AGPL-3',
+    'images': ['static/description/quick-reorder-cover.png',]
     'data': [
         'views/reorder.xml',
         'views/reorder_button.xml',
