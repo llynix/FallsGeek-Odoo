e1d875191f2e1365985cee52130e0d0e98607e7f
Llynix <ataylor@fallsgeek.com>
2024-10-17 17:16:46 -0500

    Odoo 17: Initial Commit

--- sitewide_banner/__manifest__.py
+++ sitewide_banner/__manifest__.py
@@ -5,10 +5,10 @@
         Displays a banner atop every webpage""",
     'author': "Anthony Taylor (Falls Geek)",
     'category': 'Website',
-    'version': '16.0.0.0.1',
+    'version': '17.0.0.0.1',
     'license': 'LGPL-3',
     'depends': ['website'],
-    'images': ['static/description/sitewide_banner-cover.png'],
+    'images': ['static/description/main_screenshot.png'],
     'data': [
         'views/sitewide_banner.xml',
         'views/res_config_settings_views.xml',
--- website_quick_reorder/__manifest__.py
+++ website_quick_reorder/__manifest__.py
@@ -1,15 +1,17 @@
 {
     'name': "Website Quick Reorder",
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': """Add products quickly from sale history to cart """,
     'description': """Add products quickly from sale history to cart """,
-    'author': "Anthony Taylor (Falls Geek)",
+    'author': "Falls Geek",
     'category': 'eCommerce',
     'depends': ['website_sale'],
     'license': 'AGPL-3',
-    'images': ['static/description/quick-reorder-cover.png',],
     'data': [
         'views/reorder.xml',
         'views/reorder_button.xml',
+    ],
+    'images': [
+        'static/description/main_screenshot.png',
     ],
 }
